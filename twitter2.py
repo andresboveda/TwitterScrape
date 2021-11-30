c85ba9477f8b27b18c3f8ea3b570f565a0e12d8a
Andres Boveda <84390948+andresboveda@users.noreply.github.com>
2021-11-30 11:18:10 +0100

    Add files via upload

---
+++ twitter2.py
@@ -0,0 +1,15 @@
+#documentation from https://www.youtube.com/watch?v=p8hG8MhzoYc AND ALSO https://medium.com/dataseries/how-to-scrape-millions-of-tweets-using-snscrape-195ee3594721
+import snscrape.modules.twitter as twitterScraper
+
+scraper = twitterScraper.TwitterSearchScraper('"jannik sinner" since:2021-11-18 until:2021-11-19')
+
+tweet_list = []
+
+for i,tweet in enumerate(scraper.get_items()):
+    if i > 1000:
+        break
+    tweet_list.append(tweet.content)
+    
+x = len(tweet_list)
+print("There are {} tweets on the list".format(x))
+print(tweet_list)
