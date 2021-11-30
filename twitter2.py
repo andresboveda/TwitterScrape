0122688fc3416a7167f90df5f87e163d8e3eef50
Andres Boveda <84390948+andresboveda@users.noreply.github.com>
2021-11-30 11:20:34 +0100

    Update twitter2.py

--- twitter2.py
+++ twitter2.py
@@ -1,12 +1,12 @@
 #documentation from https://www.youtube.com/watch?v=p8hG8MhzoYc AND ALSO https://medium.com/dataseries/how-to-scrape-millions-of-tweets-using-snscrape-195ee3594721
 import snscrape.modules.twitter as twitterScraper
 
-scraper = twitterScraper.TwitterSearchScraper('"jannik sinner" since:2021-11-18 until:2021-11-19')
+scraper = twitterScraper.TwitterSearchScraper('"YOUR SEARCH PHRASE" since:2021-11-18 until:2021-11-19') #modify dates as you wish
 
 tweet_list = []
 
 for i,tweet in enumerate(scraper.get_items()):
-    if i > 1000:
+    if i > 1000: #if you want to fetch more tweets, just increase the number before the break
         break
     tweet_list.append(tweet.content)
     
